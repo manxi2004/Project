27107980856583cf5c9e6acab3ff39ae9c283aa2
Jose Manuel Cardona Rondón <124000426+manxi2004@users.noreply.github.com>
2023-03-31 21:47:45 -0500

    Update Controller.java

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,41 +1,70 @@
 package model;
 
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Controller {
 
 	private Project[] projects;
+	private int numProjects;
 
 	public Controller() {
 
 		projects = new Project[10];
-	
-	}
-	
-	//Incomplete
-	public boolean RegisterProject() {
+		numProjects = 0;
 
-		return false;
 	}
 
-	//Incomplete
-	// Date class also has their own before() and after() method
-	public String searchProjectsAfterDate() {
+	public boolean registerProject(String name, String description, double budget, Calendar initialDate,
+			Calendar finalDate) {
 
+		if (numProjects < 10) {
+
+			projects[numProjects] = new Project(name, description, initialDate, finalDate, budget);
+			numProjects++;
+			return true;
+
+		} else {
+
+			return false;
+
+		}
+
+	}
+
+	public String searchProjectsAfterDate(Calendar date) {
 		String msg = "";
-
+		for (int i = 0; i < projects.length; i++) {
+			if (projects[i] != null && projects[i].getFinalDate().equals(date)) {
+				try {
+					msg += projects[i].getProjectInfo();
+				} catch (ParseException e) {
+					System.out.println("Error parsing date.");
+				}
+			}
+		}
+		if (msg.equals("")) {
+			msg = "No projects found.";
+		}
 		return msg;
-
 	}
-	
-	//Incomplete
-	// Date class also has their own before() and after() method
-	public String searchProjectsBeforeDate() {
 
+	public String searchProjectsBeforeDate(Calendar date) {
 		String msg = "";
-
+		for (int i = 0; i < projects.length; i++) {
+			if (projects[i] != null && projects[i].getInitialDate().equals(date)) {
+				try {
+					msg += projects[i].getProjectInfo();
+				} catch (ParseException e) {
+					System.out.println("Error parsing date.");
+				}
+			}
+		}
+		if (msg.equals("")) {
+			msg = "No projects found.";
+		}
 		return msg;
-
 	}
 }
+
